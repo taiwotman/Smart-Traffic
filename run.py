e5ea31934d0bb9eae40102d585a1604ab9dbfa76
Karthikaskumar <49953949+Karthikaskumar@users.noreply.github.com>
2021-08-06 21:50:20 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import tensorboard as tb
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+
 import sys
 import os
 
@@ -9,20 +12,19 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 image_path = sys.argv[1]
 
 # Read in the image_data
-image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+image_data = tf.io.gfile.GFile(image_path, 'rb').read()
 
 # Loads label file, strips off carriage return
-label_lines = [line.rstrip() for line 
-                   in tf.gfile.GFile("./pretrained_labels.txt")]
+label_lines = [line.rstrip() for line in tf.io.gfile.GFile("./pretrained_labels.txt", 'r')]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("./pretrained_graph.pb", 'rb') as f:
-    graph_def = tf.GraphDef()
+with tf.io.gfile.GFile("pretrained_graph.pb", 'rb') as f:
+    graph_def = tf.compat.v1.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
-with tf.Session() as sess:
-    # Feed the image_data as input to the graph and get first prediction
+with tf.compat.v1.Session() as sess: 
+  # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
     
     predictions = sess.run(softmax_tensor, \
@@ -35,6 +37,7 @@ with tf.Session() as sess:
         congestion_type = label_lines[node_id]
         score = predictions[0][node_id]
         if (score >=0.5):
-            print('%s (score = %.5f)' % (congestion_type, score))
+            print(congestion_type)
+            print('%s (score = %.5f))
 
         
